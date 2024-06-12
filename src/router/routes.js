053f1e4fd246c2798c1b3ca8b6426f65fbd78ebc
Luis Solano <lsolano@gruver.mx>
2024-06-12 10:55:52 -0600

    Se quita el overflow y se quita metrica "Acceso de usuarios"

--- src/router/routes.js
+++ src/router/routes.js
@@ -76,7 +76,7 @@ const routes = [
             label: 'Sistemas',
             name: 'sistemasEX',
             children: [
-              { path: 'acceoUsuarios', icono: 'group', label: 'Acceso de usuarios', name: 'acceoUsuariosEX', component: () => import('src/modules/exactor/sistemas/AccesoUsuariosModule.vue') },
+              // { path: 'acceoUsuarios', icono: 'group', label: 'Acceso de usuarios', name: 'acceoUsuariosEX', component: () => import('src/modules/exactor/sistemas/AccesoUsuariosModule.vue') },
               { path: 'informeTickets', icono: 'receipt_long', label: 'Informe OsTicket', name: 'informeTicketsEX', component: () => import('src/modules/exactor/sistemas/InformeOsTicketModule.vue') },
               // { path: 'inventarioHosts', icono: 'inventory', label: 'Inventario Hosts', name: 'inventarioHostsEX', component: () => import('src/modules/exactor/sistemas/InventarioHostsModule.vue') },
               // { path: 'relojCheck', icono: 'schedule', label: 'Reloj Check', name: 'relojCheckEX', component: () => import('src/modules/exactor/sistemas/RelojCheckModule.vue') },
