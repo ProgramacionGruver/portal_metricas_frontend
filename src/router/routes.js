2ce038ff6e9ddb79ab232f88d3ecd4141b932822
Luis Solano <lsolano@gruver.mx>
2024-08-26 16:27:39 -0600

    Ordenes de servicio para IS

--- src/router/routes.js
+++ src/router/routes.js
@@ -200,6 +200,7 @@ const routes = [
             name: 'servicioIS',
             children: [
               { path: 'csiServicio', icono: 'group', label: 'CSI y Clientes perdidos', name: 'csiServicioIS', component: () => import('src/modules/isuzu/servicio/CSIServicioModule.vue') },
+              { path: 'ordenesServicio', icono: 'receipt_long', label: 'Ordenes de Servicio', name: 'ordenesServicioIS', component: () => import('src/modules/isuzu/servicio/OrdenesServicioModule.vue') },
             ]
           }
         ]
